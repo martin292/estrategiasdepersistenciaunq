9851031f6eb0231ab78050331f8cd3cc9ca3ae50
Martin.Ramos292@gmail.com <Martin.Ramos292@gmail.com@8d55ede9-e4d5-74a6-4f62-28df10182210>
2014-03-29 12:41:32 +0000

    Actualizado

---
+++ tp1/AterrizarDB.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE `AterrizarDB`;
+
+CREATE TABLE `usuarios` (
+  `username` varchar(45) NOT NULL AUTO_INCREMENT,
+  `password` varchar(45) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `fecha` date DEFAULT NULL,
+
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
--- tp1/src/main/java/database/DBConnector.java
+++ tp1/src/main/java/database/DBConnector.java
@@ -1,4 +1,4 @@
-package tp1;
+package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
